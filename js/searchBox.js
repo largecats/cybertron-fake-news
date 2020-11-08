cbe2b9e63c00452b256e8db55d2d73a5a820cc2a
largecats <linfanxiaolinda@outlook.com>
2020-11-08 18:18:39 +0800

    updated

--- js/searchBox.js
+++
@@ -1,9 +0,0 @@
-SimpleJekyllSearch({
-    searchInput: document.getElementById('search-input'),
-    resultsContainer: document.getElementById('results-container'),
-    json: '/search.json',
-    searchResultTemplate: '<li><a href="{url}" title="{desc}">{title}</a></li>',
-    noResultsText: 'No article found',
-    limit: 20, // maximum number of articles returned
-    fuzzy: false // fuzzy matching
-})
